8a7d3d50b8be13d057d9540299d495442611e07a
themartinlopez <code.mlopez@gmail.com>
2022-06-29 16:39:05 -0500

    more changes to backend

--- src/main/java/mlopez/awsupload/Main.java
+++ src/main/java/mlopez/awsupload/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsUploadApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsUploadApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
--- src/main/java/mlopez/awsupload/filestore/FileStore.java
+++ src/main/java/mlopez/awsupload/filestore/FileStore.java
@@ -3,9 +3,12 @@ package mlopez.awsupload.filestore;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
@@ -34,6 +37,15 @@ public class FileStore {
             s3.putObject(path, fileName, inputStream, metadata);
         } catch (AmazonServiceException e) {
             throw new IllegalStateException("Failed to store to s3", e);
+        }
+    }
+
+    public byte[] download(String path, String key) {
+        try {
+            S3Object object = s3.getObject(path, key);
+            return IOUtils.toByteArray(object.getObjectContent());
+        } catch (AmazonServiceException | IOException e) {
+            throw new IllegalStateException("Failed to download file to s3", e);
         }
     }
 }
