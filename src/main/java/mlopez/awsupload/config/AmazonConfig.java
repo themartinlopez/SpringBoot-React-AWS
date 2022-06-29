d526cfa731f2396db620f6c58714e3899644f05e
themartinlopez <code.mlopez@gmail.com>
2022-06-29 18:37:31 -0500

    removed static aws access, now using AWS Toolkit Credentials

--- src/main/java/mlopez/awsupload/config/AmazonConfig.java
+++ src/main/java/mlopez/awsupload/config/AmazonConfig.java
@@ -1,8 +1,10 @@
 package mlopez.awsupload.config;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +15,17 @@ public class AmazonConfig {
 
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(
+        /*AWSCredentials awsCredentials = new BasicAWSCredentials(
                 "",
                 ""
-        );
+        );*/
+
+        AWSCredentials awsCredentials = new ProfileCredentialsProvider().getCredentials();
+
         return AmazonS3ClientBuilder
                 .standard()
+                .withRegion(Regions.US_EAST_1)
+                //.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
